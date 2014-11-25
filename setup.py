e0ae9bac43748ade5c8bf7d031437e49f9ee0c60
Bharat Medasani <mbkumar@gmail.com>
2014-11-24 20:48:22 -0800

    setup.py creates standalone executables

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import os
+import glob
 
 from setuptools import setup
 
 SETUP_PTH = os.path.dirname(os.path.abspath(__file__))
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 setup(
         name="pydii",
@@ -14,6 +19,7 @@ setup(
         description="PyDII is a python tool to evaluate point defect " 
                   "concentrations and micro-alloy solute site preference "
                   "in intermetallics",
+        long_description=readme(),
         classifiers=[
             "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 2.7",
@@ -28,6 +34,22 @@ setup(
             "Topic :: Software Development :: Libraries :: Python Modules"
             ],
         license="MIT",
-        scripts=glob.glob(os.path.join(SETUP_PTH, "scripts", "*"))
+        scripts=glob.glob(os.path.join(SETUP_PTH, "scripts", "*")),
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        entry_points={
+            'console_scripts':[
+                'gen_def_structure=scripts.gen_def_structure.'
+                               'im_vac_antisite_def_struct_gen',
+                'gen_def_energy=scripts.gen_def_energy.'
+                               'im_vac_antisite_def_energy_parse',
+                'gen_def_profile=scripts.gen_def_profile.'
+                               'im_vac_antisite_def_profile',
+                'gen_sol_pref_structure=scripts.gen_def_structure.'
+                               'im_sol_sub_def_struct_gen',
+                'gen_sol_def_energy=scripts.gen_def_energy.'
+                               'im_sol_sub_def_energy_parse',
+                'gen_sol_site_pref=scripts.gen_def_profile.'
+                               'im_sol_sub_def_profile']}
 )
 
